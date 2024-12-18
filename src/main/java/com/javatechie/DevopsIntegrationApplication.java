4c258f5c8de917e5393cecfe98063b34eb46696a
Anoop Joy <64806470+anoopkoomullil@users.noreply.github.com>
2024-12-18 13:00:50 +0000

    committing changes Jenk latest

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie Anoop Joy";
+		return "welcome to javatechie Anoop Joy latest";
 	}
 
 	public static void main(String[] args) {
