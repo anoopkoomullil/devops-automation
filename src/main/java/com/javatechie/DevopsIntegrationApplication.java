2f2b186533b934cf4a207e601833e5a65da9f81e
anoopkoomullil <64806470+anoopkoomullil@users.noreply.github.com>
2024-12-18 12:06:45 +0000

    committing again anoop joy

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie anoop";
+		return "welcome to javatechie anoop joy";
 	}
 
 	public static void main(String[] args) {
